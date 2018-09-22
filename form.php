2b54596267e30a8b027071fe5eb966eb9663a3c3
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2018-09-22 10:59:13 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ivania Nonita C</title>
+</head>
+<body>
+<?php
+include 'header.php';
+?>
+<form action="hal1.php" method="POST">
+	Nama <input type="text" name="nama"><br>
+	Akses <input type="text" name="akses"><br><br><br>
+	<input type="submit" name="submit" value="kirim "><br>
+</form>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>6701174057</title>
+</head>
+<body bgcolor="teal">
+	<div style="background-color: grey;">
+		<marquee>
+			<img src="1.jpg" width="50">
+			<font size="10" color="white">Welcome to Website</font>
+			<img src="1.jpg" width="50">
+		</marquee>
+		<hr>
+	</div>
+</body>
+</html>
